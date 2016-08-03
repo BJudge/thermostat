9a745c425c54c0ccb96e5a77e73848c47cef9d46
Nomi Vos <nvoswork@gmail.com>
2016-08-03 09:48:46 -0500

    sets minimum temperature. tests passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -19,6 +19,14 @@ describe('Thermostat', function() {
 
   it('can decrease the temperature by 1 degree', function() {
     thermostat.down();
-    expect(thermostat.getCurrentTemperature()).toEqual(19)
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('cannot go below 10 degrees', function() {
+    for(var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+    //expect(function(){thermostat.getCurrentTemperature();}).toThrowError('Minimum temperature reached');
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,8 @@
 'use strict';
 
-function Thermostat(){
+function Thermostat() {
 this.temperature = 20;
+this.MINIMUM_TEMPERATURE = 10;
 }
 Thermostat.prototype.getCurrentTemperature = function() {
   return this.temperature;
@@ -9,6 +10,13 @@ Thermostat.prototype.getCurrentTemperature = function() {
 Thermostat.prototype.up = function () {
    this.temperature += 1;
 };
+Thermostat.prototype.isMinTemperature = function() {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+}
+
 Thermostat.prototype.down = function () {
+  if(this.isMinTemperature()) {
+    return;
+  }
   this.temperature -= 1;
 };
